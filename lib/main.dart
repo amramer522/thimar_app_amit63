a212099c42dedb8d831aee8238f7ecf6f8e7130e
AmrBakr1211 <a.amer.eg@dataocean.sa>
2024-06-08 22:42:44 +0300

    firebase auth - google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:app/core/app_theme.dart';
+import 'package:app/views/test_firebase.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/logic/cache_helper.dart';
 import 'core/logic/helper_methods.dart';
 import 'features/service_locator.dart';
+import 'firebase_options.dart';
 import 'views/auth/login.dart';
 import 'views/auth/otp.dart';
 import 'views/auth/register.dart';
@@ -16,8 +19,11 @@ void main() async {
   await CacheHelper.init();
   await EasyLocalization.ensureInitialized();
   initServiceLocator();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(EasyLocalization(
-    supportedLocales: const [Locale("en"), Locale("ar")],
+    supportedLocales: const [Locale("en"), Locale("ar"),],
     path: "assets/translations",
     fallbackLocale: const Locale("en"),
     startLocale: const Locale("ar"),
@@ -32,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
-      child: HomeView(),
+      child: TestFirebaseView(),
       builder: (context, child) => MaterialApp(
         title: 'Thimar',
         theme: AppTheme.light,
---
+++ lib/views/test_firebase.dart
@@ -0,0 +1,185 @@
+import 'package:app/core/design/app_button.dart';
+import 'package:app/core/design/app_input.dart';
+import 'package:app/core/logic/helper_methods.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class TestFirebaseView extends StatefulWidget {
+  const TestFirebaseView({super.key});
+
+  @override
+  State<TestFirebaseView> createState() => _TestFirebaseViewState();
+}
+
+class _TestFirebaseViewState extends State<TestFirebaseView> {
+  final emailController = TextEditingController(text: "amramer522@gmail.com");
+  final passwordController = TextEditingController(text: "123456789");
+  bool isLoading = false;
+  final signInObject = GoogleSignIn();
+  GoogleSignInAccount? account;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(account?.displayName ?? "No User Founded"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await signInObject.signOut();
+              account=null;
+              showMessage("Log out Success");
+              setState(() {});
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                AppInput(
+                  label: "البريد الإلكتروني",
+                  controller: emailController,
+                ),
+                AppInput(
+                  label: "كلمة المرور",
+                  inputType: InputType.password,
+                  controller: passwordController,
+                ),
+                AppButton(
+                  isLoading: isLoading,
+                  onPressed: () async {
+
+                  },
+                  text: "تسجيل الدخول",
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                OutlinedButton(
+                  onPressed: () async {
+                   loginWithGoogle();
+                  },
+                  child: Text("Login With Google"),
+                ),
+                OutlinedButton(
+                  onPressed: () async {
+                    registerFirebase();
+                    },
+                  child: Text("SignUp With Google"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  Future<void> registerWithGoogle()async{
+    try {
+      account = await signInObject.signIn();
+      showMessage("Hello ${account?.displayName}",
+          messageType: MessageType.success);
+      // print(account?.email+account.);
+
+    } catch (ex) {
+      print("catch*******");
+      showMessage(ex.toString());
+    }
+    setState(() {});
+  }
+  Future<void> loginWithGoogle()async{
+    try {
+      account = await signInObject.signIn();
+      showMessage("Hello ${account?.displayName}",
+          messageType: MessageType.success);
+      print(account?.displayName);
+
+    } catch (ex) {
+      print("catch*******");
+      showMessage(ex.toString());
+    }
+    setState(() {});
+  }
+
+  Future<void> loginFirebase()async{
+    isLoading = true;
+    setState(() {});
+    try {
+      final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+
+      showMessage("Hello ${userCredential.user?.displayName}",
+          messageType: MessageType.success);
+    } on FirebaseAuthException catch (ex) {
+      print("FirebaseAuthException*******");
+      print(ex.message);
+      print(ex.code);
+      showMessage(ex.code ?? "Failed");
+    } catch (ex) {
+      print("catch*******");
+      showMessage(ex.toString());
+    }
+
+    isLoading = false;
+    setState(() {});
+  }
+  Future<void> registerFirebase()async{
+    isLoading = true;
+    setState(() {});
+    try {
+      final userCredential = await FirebaseAuth.instance.signInWithProvider(
+        GoogleAuthProvider()
+      );
+
+      showMessage("Hello ${userCredential.user?.displayName}",
+          messageType: MessageType.success);
+    } on FirebaseAuthException catch (ex) {
+      print("FirebaseAuthException*******");
+      print(ex.message);
+      print(ex.code);
+      showMessage(ex.code ?? "Failed");
+    } catch (ex) {
+      print("catch*******");
+      showMessage(ex.toString());
+    }
+
+    isLoading = false;
+    setState(() {});
+  }
+  Future<void> registerFirebaseEmailAndPassword()async{
+    isLoading = true;
+    setState(() {});
+    try {
+      final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+
+      showMessage("Hello ${userCredential.user?.displayName}",
+          messageType: MessageType.success);
+    } on FirebaseAuthException catch (ex) {
+      print("FirebaseAuthException*******");
+      print(ex.message);
+      print(ex.code);
+      showMessage(ex.code ?? "Failed");
+    } catch (ex) {
+      print("catch*******");
+      showMessage(ex.toString());
+    }
+
+    isLoading = false;
+    setState(() {});
+  }
+
+}
